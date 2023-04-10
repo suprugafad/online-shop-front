3e9eaa7a609433aaa6e293699f9ae9a08989bb22
Alexandrina <poidaalesya382@gmail.com>
2023-04-10 20:30:46 +0300

    Add signin and signup pages and connect authorization from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import SignInPage from './pages/SignIn';
+import SignUpPage from './pages/SignUp';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <CssBaseline />
+      <Routes>
+        <Route path='/' element={<SignInPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+      </Routes>
+    </Router>
   );
 }
 
